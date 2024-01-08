30cc8e5dca79a62d6abba40352421bcb064100b6
Hampus Hallkvist <hampus@hallkvist.org>
2024-01-08 20:12:00 +0100

    docs: Improved example for redux

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
-import { StorageProvider } from "@hampfh/use-storage"
+import { Provider } from "react-redux"
+import { store } from "./store.ts"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<StorageProvider>
+		<Provider store={store}>
 			<App />
-		</StorageProvider>
+		</Provider>
 	</React.StrictMode>
 )
